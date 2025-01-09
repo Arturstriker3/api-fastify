9ce4985f34d070ed08699c1f32d3db0f962b7084
Arturstriker3 <artur.danielgama@gmail.com>
2025-01-09 02:20:41 -0300

    ✨ feat: Add ESLint and Prettier configuration files, enhance code formatting and linting rules

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,34 +1,41 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
-@Entity("users")
+@Entity('users')
 export class User {
-    @PrimaryGeneratedColumn("uuid")
-    id!: string;
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 
-    @Column("varchar")
-    name: string;
+  @Column('varchar')
+  name: string;
 
-    @Column("varchar", { unique: true })
-    email: string;
+  @Column('varchar', { unique: true })
+  email: string;
 
-    @Column("varchar")
-    password: string;
+  @Column('varchar')
+  password: string;
 
-    @CreateDateColumn({ type: "timestamp" })
-    created_at: Date;
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
 
-    @UpdateDateColumn({ type: "timestamp" })
-    updated_at: Date;
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
 
-    @DeleteDateColumn({ type: "timestamp" })
-    deleted_at: Date;
+  @DeleteDateColumn({ type: 'timestamp' })
+  deleted_at: Date;
 
-    constructor() {
-        this.name = "";
-        this.email = "";
-        this.password = "";
-        this.created_at = new Date();
-        this.updated_at = new Date();
-        this.deleted_at = new Date();
-    }
+  constructor() {
+    this.name = '';
+    this.email = '';
+    this.password = '';
+    this.created_at = new Date();
+    this.updated_at = new Date();
+    this.deleted_at = new Date();
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,19 @@ import { UserController } from './controllers/UserController';
 import { z } from 'zod';
 
 export async function routes(app: FastifyTypedIntance): Promise<void> {
-  app.post('/register', {
-    schema: {
-      tags: ['users'],
-      description: 'Create a new user',
-      body: z.object({
-        name: z.string().min(1, 'Name is required'),
-        email: z.string().email('Invalid email format'),
-        password: z.string().min(6, 'Password must be at least 6 characters'),
-      }),
+  app.post(
+    '/register',
+    {
+      schema: {
+        tags: ['users'],
+        description: 'Create a new user',
+        body: z.object({
+          name: z.string().min(1, 'Name is required'),
+          email: z.string().email('Invalid email format'),
+          password: z.string().min(6, 'Password must be at least 6 characters'),
+        }),
+      },
     },
-  }, UserController.createUser);
+    UserController.createUser
+  );
 }
